4462e06882c8a52fd5117e951cdcfa82e6c70651
NsSumse <stefanduesing@googlemail.com>
2016-06-27 16:10:17 +0200

    Update seneca.js

--- seneca.js
+++ seneca.js
@@ -1,3 +1,21 @@
-/**
- * Created by Duesing_S on 27.06.2016.
- */
+var seneca = require('seneca')();
+var Promise = require("bluebird");
+
+
+seneca.addGen = function add(pattern, fn) {
+    seneca.add(pattern, function (args, done) {
+        Promise.coroutine(fn)().then(function (res) {
+            if (res.toJSON) {
+                done(null, res.toJSON());
+            } else {
+                done(null, res);
+            }
+        }).catch(function (err) {
+            done(null, {err:err});
+        })
+    });
+};
+
+seneca.actGen = Promise.promisify(seneca.act, {context: seneca});
+
+module.exports = seneca;
